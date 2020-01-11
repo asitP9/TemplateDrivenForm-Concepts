0b5e4119caf93426216ab180ad6254b8b67accbc
asitp9 <s3753288@student.rmit.edu.au>
2020-01-11 20:06:46 +1100

    added therest property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  submitted:boolean=false;
+  genders:string[]=["Male","Female"];
+  defaultGender:string=this.genders[1];
+  @ViewChild('f',{static:true}) signUpForm:NgForm;
+  defaultQuestion:string="teacher";
+  questionAnswer:string;
+  outputFormDetails={
+    username:"",
+    email:"",
+    secretQuestion:"",
+    answer:"",
+    gender:""
+  }
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:'asit.p9@gmail.com'
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'My name is anthony gonsalves',
+    //   gender:this.genders[0]
+    // })
+
+    // OR
+
+    // this.signUpForm.form.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:'asit.p9@gmail.com'
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'My name is anthony gonsalves',
+    //   gender:this.genders[0]
+    // })
+
+    this.signUpForm.form.patchValue({
+      userData:{
+        username:suggestedName        
+      },
+     
+    })
+
   }
+
+  // submitForm(formElement:NgForm){
+  //   console.log(formElement);
+  // }
+
+  submitForm(){
+    this.submitted=true;
+      console.log(this.signUpForm);
+
+      this.outputFormDetails.username=this.signUpForm.value.userData.username;
+      this.outputFormDetails.email=this.signUpForm.value.userData.email;
+      this.outputFormDetails.secretQuestion=this.signUpForm.value.secret;
+      this.outputFormDetails.answer=this.signUpForm.value.questionAnswer;
+      this.outputFormDetails.gender=this.signUpForm.value.gender;
+    }
 }
